e43c0350aff390e925616c2222e3bd0b2737c665
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-29 13:34:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ end
 
 def monopoly_with_third_tier
   monopoly = monopoly_with_second_tier
-  monopoly[:railroads][:rent_in_dollars] = {:one_piece_owned => 25,:two_pieces_owned => 50,:threee_pieces_owned => 100,:four_pieces_owned => 200}
+  monopoly[:railroads][:rent_in_dollars] = {:one_piece_owned => 25,:two_pieces_owned => 50,:three_pieces_owned => 100,:four_pieces_owned => 200}
   monopoly[:railroads][:names] = {:reading_railroad => {},:pennsylvania_railroad => {},:b_and_o_railroad => {},:shortline_railroad => {}}
   monopoly
 end
